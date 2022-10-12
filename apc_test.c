6d7623e477caee4f257b12052d7d2cbd9545f0b3
rishabh khemka <rishabhkhemu@hotmail.com>
2022-10-12 15:38:37 +0530

    added testing structure

--- apc_test.c
+++ apc_test.c
@@ -1,13 +1,39 @@
 #include "apc_test.h"
-
-Sample data [50];
+#include "apc.h"
+#define NUM_TESTCASES 10;
+Sample addition_tests[NUM_TESTCASES];
+Sample subraction_tests[NUM_TESTCASES];
 
 void create_test ()
 {
 	/* Addition Test Cases */
-	data [0] = {"123456", '+', '+', "987654", '+', "1111110", '+'};
-	data [1] = {"123", '-', '+', "25", '+', "98", '-'};
+	addition_cases[0] = {"123456", '+', '+', "987654", '+', "1111110", '+'};
+	addition_cases[1] = {"123", '-', '+', "25", '+', "98", '-'};
 
 	/*Subtraction Test Cases */
-	data [2] = {"1000000", '-', "1", "999999"};
-} 
+	subtraction_cases[2] = {"1000000", '-', "1", "999999"};
+}
+
+
+void RunAllTests() {
+	int totalPassedCases = 0;
+	totalPassedCases += runTests(addition_cases, addition);
+	totalPassedCases += runTests(subtraction_cases, subtraction);
+	printf("\npassed %d out of %d cases\n", totalPassedCases, NUM_TESTCASES*2);
+}
+
+typedef int (*operation) (Dlist**, Dlist**, Dlist**, Dlist**, Dlist**, Dlist**);
+
+// runs tests for given sample test cases and operation 
+// operation - aka function under test (fut)
+void runTests(sample[] inputArray, operation fut) {
+	int casesPassed = 0;
+	for (int i = 0; i < NUM_TESTCASES; i++) {
+		input = inputArray[i];
+		// convert to ll and call the operation
+		// int result  = *fut(input.op1, input.op2, input.Operator);
+		// compare result and expected output in case, increment cases passed
+		// pretty print accordingly - both positive and negative scenarios
+	}
+	return casesPassed;
+}
